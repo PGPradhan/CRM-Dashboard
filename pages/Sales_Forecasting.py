c926a9e67faeba69d96ee98035ded0d47bf8f723
PGPradhan <pradhan.prathamesh10@gmail.com>
2024-04-09 22:51:53 +0530

    Committt

--- pages/Sales_Forecasting.py
+++ pages/Sales_Forecasting.py
@@ -6,6 +6,9 @@ import seaborn as sn
 import warnings
 import streamlit as st
 warnings.filterwarnings('ignore')
+import os
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from statsmodels.tsa.stattools import adfuller
@@ -23,10 +26,10 @@ f1 = st.file_uploader(":file_folder: Upload a File ", type=(["csv", "text", "xls
 if f1 is not None:
     filename = f1.name
     st.write(filename)
-    lead = pd.read_csv(f1, encoding='unicode_escape')
+    df = pd.read_csv(f1, encoding='unicode_escape')
 else:
     os.chdir(r"E:\Projects\CRM\pages")
-    lead = pd.read_csv("train.csv", encoding='unicode_escape')
+    df = pd.read_csv("train.csv", encoding='unicode_escape')
 
 
 # Filter records for store 1 and item 1 -> to be able to scale to other items in the future
@@ -49,4 +52,186 @@ test_df = temp_df.loc['2017-10-01':].reset_index(drop=False)
 
 plot = sn.lineplot(x='date', y='sales', data=df)
 _ = plot.set(title='Sales for Store 1, Item 1 over the years')
-st.pyplot(plot)
+
+# # Save the figure
+# plot.figure.savefig('sales_plot.png')
+#
+# # Display the saved image in Streamlit
+# st.image('sales_plot.png')
+
+# Subtract 1 year (365 days) from test data
+dates = (test_df['date'] - pd.to_timedelta(365, unit='D')).values.astype('datetime64[D]')
+seasonal_naive_sales = train_df[train_df['date'].isin(dates)]['sales']
+
+# make a copy of the test_df and make naive predictions for the last 3 months of 2017
+sn_pred_df = test_df.copy().drop('sales', axis=1)
+sn_pred_df['seasonal_naive_sales'] = pd.DataFrame(seasonal_naive_sales).set_index(test_df.index)
+
+plt.figure(figsize=(14,7))
+plt.plot(train_df['date'], train_df['sales'], label='Train')
+plt.plot(test_df['date'], test_df['sales'], label='Test')
+plt.plot(sn_pred_df['date'], sn_pred_df['seasonal_naive_sales'], label='Forecast - Seasonal Naive')
+plt.legend(loc='best')
+plt.xlabel('date')
+plt.ylabel('sales')
+plt.title('Forecasts using Baseline Model: Seasonal Naive')
+plt.show()
+
+# st.pyplot(plt)
+
+errors_df = pd.merge(test_df, sn_pred_df, on='date')
+errors_df = errors_df[['date', 'sales', 'seasonal_naive_sales']]
+errors_df = pd.merge(test_df, sn_pred_df, on='date')
+errors_df = errors_df[['date', 'sales', 'seasonal_naive_sales']]
+errors_df['errors'] = test_df['sales'] - sn_pred_df['seasonal_naive_sales']
+errors_df.insert(0, 'model', 'Seasonal Naive')
+
+def mae(err):
+    return np.mean(np.abs(err))
+
+def rmse(err):
+    return np.sqrt(np.mean(err ** 2))
+
+def mape(err, sales=errors_df['sales']):
+    return (np.sum(np.abs(err))/np.sum(sales) * 100)
+
+result_df = errors_df.groupby('model').agg(total_sales=('sales', 'sum'),
+                                           total_sn_pred_sales=('seasonal_naive_sales', 'sum'),
+                                           overall_error=('errors', 'sum'),
+                                           MAE=('errors', mae),
+                                           RMSE=('errors', rmse),
+                                           MAPE=('errors', mape))
+
+
+plt.figure(figsize=(14,7))
+plt.plot(errors_df['date'], np.abs(errors_df['errors']), label='errors')
+plt.plot(errors_df['date'], errors_df['sales'], label='actual sales')
+plt.plot(errors_df['date'], errors_df['seasonal_naive_sales'], label='forecast')
+plt.legend(loc='best')
+plt.xlabel('date')
+plt.ylabel('sales')
+plt.title('Seasonal Naive forecasts with actual sales and errors')
+plt.show()
+st.pyplot(plt)
+
+print(result_df)
+
+
+ts_decomp_df = train_df.set_index('date') # set date as index
+ts_decomp_df['sales'] = ts_decomp_df['sales'].astype(float)
+
+result = seasonal_decompose(ts_decomp_df['sales'], model='additive', period=365)
+# fig = plt.figure()
+# fig = result.plot()
+# fig.set_size_inches(14, 12)
+# st.pyplot(fig)
+
+# Assuming train_df and test_df are your training and test DataFrames
+hw_train_df = train_df[['date', 'sales']].set_index('date')
+hw_test_df = test_df[['date', 'sales']].set_index('date')
+
+# Apply Triple Exponential Smoothing
+
+hw_model_1 = ExponentialSmoothing(hw_train_df, trend='add', seasonal='add', seasonal_periods=12, use_boxcox=False)
+hw_fit_1 = hw_model_1.fit(remove_bias=False)
+pred_fit_1 = pd.Series(hw_fit_1.predict(start=hw_test_df.index[0], end=hw_test_df.index[-1]),
+                       name='pred_sales').reset_index()
+
+hw_model_2 = ExponentialSmoothing(hw_train_df, trend='add', seasonal='add', seasonal_periods=12, damped=True, use_boxcox=False)
+hw_fit_2 = hw_model_2.fit(remove_bias=False)
+pred_fit_2 = pd.Series(hw_fit_2.predict(start=hw_test_df.index[0], end=hw_test_df.index[-1]),
+                       name='pred_sales').reset_index()
+
+# Merge predictions and actual sales into one DataFrame
+errors_df_hw1 = pd.merge(test_df, pred_fit_1, left_on='date', right_on='index')
+errors_df_hw1 = errors_df_hw1[['date', 'sales', 'pred_sales']]
+errors_df_hw1['errors'] = errors_df_hw1['sales'] - errors_df_hw1['pred_sales']
+errors_df_hw1.insert(0, 'model', 'Holt-Winters (No Damping)')
+
+errors_df_hw2 = pd.merge(test_df, pred_fit_2, left_on='date', right_on='index')
+errors_df_hw2 = errors_df_hw2[['date', 'sales', 'pred_sales']]
+errors_df_hw2['errors'] = errors_df_hw2['sales'] - errors_df_hw2['pred_sales']
+errors_df_hw2.insert(0, 'model', 'Holt-Winters (Damped)')
+
+# Evaluate the predictions
+result_df_hw = pd.concat([errors_df_hw1, errors_df_hw2])
+result_df_hw.reset_index(drop=True, inplace=True)
+
+# Merge predictions and actual sales into one df
+errors_df_hw = pd.merge(test_df, pred_fit_1, left_on='date', right_on='index')
+errors_df_hw = errors_df_hw[['date', 'sales', 'pred_sales']]
+errors_df_hw['errors'] = errors_df_hw.sales - errors_df_hw.pred_sales
+errors_df_hw.insert(0, 'model', 'Holt-Winters')
+
+
+# Evaluate the predictions for Holt-Winters without damping trend component
+plt.figure(figsize=(14,7))
+plt.plot(train_df['date'], train_df['sales'], label='Train')
+plt.plot(test_df['date'], test_df['sales'], label='Test')
+plt.plot(errors_df_hw['date'], errors_df_hw['pred_sales'], label='Forecast - HW no damping')
+plt.legend(loc='best')
+plt.xlabel('date')
+plt.ylabel('sales')
+plt.title('Forecasts using Holt-Winters without damping trend component')
+plt.show()
+# st.pyplot(plt)
+
+plt.figure(figsize=(14,7))
+plt.plot(errors_df_hw['date'], np.abs(errors_df_hw['errors']), label='errors')
+plt.plot(errors_df_hw['date'], errors_df_hw['sales'], label='actual sales')
+plt.plot(errors_df_hw['date'], errors_df_hw['pred_sales'], label='forecast')
+plt.legend(loc='best')
+plt.xlabel('date')
+plt.ylabel('sales')
+plt.title('Holt-Winters forecasts with actual sales and errors')
+plt.show()
+st.pyplot(plt)
+
+result_df_hw = errors_df_hw.groupby('model').agg(total_sales=('sales', 'sum'),
+                                          total_pred_sales=('pred_sales', 'sum'),
+                                          holt_winters_overall_error=('errors', 'sum'),
+                                          MAE=('errors', mae),
+                                          RMSE=('errors', rmse),
+                                          MAPE=('errors', mape))
+print(result_df_hw)
+
+# Merge predictions and actual sales into one df
+errors_df_hwd = pd.merge(test_df, pred_fit_2, left_on='date', right_on='index')
+errors_df_hwd = errors_df_hwd[['date', 'sales','pred_sales']]
+errors_df_hwd['errors'] = errors_df_hwd.sales - errors_df_hwd.pred_sales
+errors_df_hwd.insert(0, 'model', 'Holt-Winters-Damped')
+
+
+plt.figure(figsize=(14,7))
+plt.plot(errors_df_hwd['date'], np.abs(errors_df_hwd['errors']), label='errors')
+plt.plot(errors_df_hwd['date'], errors_df_hwd['sales'], label='actual sales')
+plt.plot(errors_df_hwd['date'], errors_df_hwd['pred_sales'], label='forecast')
+plt.legend(loc='best')
+plt.xlabel('date')
+plt.ylabel('sales')
+plt.title('Holt-Winters (damping) forecasts with actual sales and errors')
+plt.show()
+st.pyplot(plt)
+
+result_df_hwd = errors_df_hwd.groupby('model').agg(total_sales=('sales', 'sum'),
+                                          total_pred_sales=('pred_sales', 'sum'),
+                                          holt_winters_overall_error=('errors', 'sum'),
+                                          MAE=('errors', mae),
+                                          RMSE=('errors', rmse),
+                                          MAPE=('errors', mape))
+print(result_df_hwd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
